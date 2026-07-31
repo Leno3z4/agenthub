7d1b725d1eef55d8919d9a8195e58e7473c1c117
Leno3z4 <leno3z4rrrri@gmail.com>
2026-08-01 00:17:14 +0100

    Create contracts.js

---
+++ frontendd/lib/contracts.js
@@ -0,0 +1,81 @@
+import {
+  getContract,
+} from "viem";
+
+export const USDC_ABI = [
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        type: "bool",
+      },
+    ],
+  },
+];
+
+export const TOKEN_MESSENGER_ABI = [
+  {
+    type: "function",
+    name: "depositForBurn",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        name: "destinationDomain",
+        type: "uint32",
+      },
+      {
+        name: "mintRecipient",
+        type: "bytes32",
+      },
+      {
+        name: "burnToken",
+        type: "address",
+      },
+      {
+        name: "destinationCaller",
+        type: "bytes32",
+      },
+    ],
+    outputs: [],
+  },
+];
+
+export function getUsdcContract(
+  walletClient,
+) {
+  return getContract({
+    address:
+      process.env
+        .NEXT_PUBLIC_ARC_USDC_ADDRESS,
+    abi: USDC_ABI,
+    client: walletClient,
+  });
+}
+
+export function getTokenMessengerContract(
+  walletClient,
+) {
+  return getContract({
+    address:
+      process.env
+        .NEXT_PUBLIC_CCTP_TOKEN_MESSENGER,
+    abi: TOKEN_MESSENGER_ABI,
+    client: walletClient,
+  });
+}

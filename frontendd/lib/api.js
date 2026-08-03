152c7ad9da19faea7158cbc3c32b15c06a309c46
leo <leno3z4rrrri@gmail.com>
2026-08-03 18:41:27 +0100

    Update api.js

--- frontendd/lib/api.js
+++ frontendd/lib/api.js
@@ -1,6 +1,6 @@
 export const BACKEND_URL =
   process.env.NEXT_PUBLIC_BACKEND_URL ||
-  "http://localhost:8000";
+  "https://agenthub-wine.vercel.app/";
 
 // ----------------------------------------------------
 // Wallet

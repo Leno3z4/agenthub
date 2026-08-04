f6f16a18074cd8b67df8261049593f6bed1b08ad
leo <leno3z4rrrri@gmail.com>
2026-08-04 18:55:23 +0100

    Update api.js

--- frontendd/lib/api.js
+++ frontendd/lib/api.js
@@ -1,6 +1,6 @@
 export const BACKEND_URL =
   process.env.NEXT_PUBLIC_BACKEND_URL ||
-  "https://agenthub-g0m8.onrender.com/";
+  "https://agenthub-wine.vercel.app";
 
 // ----------------------------------------------------
 // User

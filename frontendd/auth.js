7138b6b5b5be443c8abd79d29caf6de3f8f935fb
leo <leno3z4rrrri@gmail.com>
2026-08-08 18:45:27 +0100

    Update auth.js

--- frontendd/auth.js
+++ frontendd/auth.js
@@ -36,7 +36,7 @@ export const {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            google_id: profile.sub,
+            provider: account.provider,
             email: profile.email,
             name: profile.name,
             picture: profile.picture,
